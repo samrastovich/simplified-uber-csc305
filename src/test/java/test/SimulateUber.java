1557027b61074c35b2d4c3eb1c6d10aa50b80c12
samrastovich <samrastovich@Sams-MacBook-Pro.local>
2017-10-10 11:37:24 -0700

    Started testing with drivers and passengers

--- src/test/java/test/SimulateUber.java
+++ src/test/java/test/SimulateUber.java
@@ -1,5 +1,21 @@
 package test;
 
-public class SimulateUber {
+import model.Driver;
+import model.Passenger;
+import model.Person;
 
+public class SimulateUber implements Runnable {
+	
+	public void run() {
+		Driver tim = new Driver("Tim", "Smith");
+		Driver dave = new Driver("Dave", "Barkley");
+		Driver dylan = new Driver("Dylan", "Broseph");
+		Driver michelle = new Driver("Michelle", "Rider");
+			
+		Person chad = new Passenger("Chad", "Grep");
+		chad.setBalance(45);
+		
+		
+	}
+	
 }
